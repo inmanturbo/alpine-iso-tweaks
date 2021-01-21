d3ff515ba4b7480f0000e09fb5cde4e716897808
inmanturbo <inman.turbo@gmail.com>
2021-01-20 23:55:37 -0500

    wip

--- extract.sh
+++ extract.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ORIGINAL_ISO=${1}
+CONTENTS_DIR="${2:-iso-contents}"
 
 docker build -t isobuilder .
 
 docker run -v ${ORIGINAL_ISO}:/tmp/distro.iso -v $(pwd):/root isobuilder \
   osirrox \
     -indev /tmp/distro.iso \
-    -extract / /root/iso-contents
+    -extract / /root/${CONTENTS_DIR}
