8e3cabcb420b86e53aa20dfa25924dd9845c93ce
inmanturbo <inman.turbo@gmail.com>
2021-01-23 13:39:16 -0500

    `wip

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ docker build -t isobuilder .
 docker run -v $(pwd):/root isobuilder \
   mkisofs -o /root/${OUTFILE}.iso \
   -b boot/syslinux/isolinux.bin \
-  -c boot.cat -no-emul-boot \
+  -c boot/syslinux/boot.cat -no-emul-boot \
   -boot-load-size 4 \
   -boot-info-table -J -R -V "Custom ISO Preseed" \
   /root/${INFILE}
